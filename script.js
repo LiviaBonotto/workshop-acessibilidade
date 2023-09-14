d03fe1401a098be02f3dc214739e5a9781e9d6c5
Lívia Bonotto <liviabonotto@gmail.com>
2023-09-14 20:03:55 -0300

    Workshop

--- script.js
+++ script.js
@@ -17,4 +17,62 @@ tabs.forEach(tab => {
         tabContent.classList.add('is-visible');
         tab.classList.add('is-active');
     });
+
+    tab.addEventListener('keydown', (event) => {
+        event.preventDefault();
+
+        const {key} = event;
+        const activeTab = event.target;
+
+        if (key === 'ArrowLeft'){
+            let nextTab = activeTab.previousElementSibling;
+
+            if (!nextTab) {
+                nextTab = activeTab.parentElement.lastElementChild;
+            }
+
+            activeTab.tabIndex = -1;
+            nextTab.tabIndex = 0;
+            nextTab.focus();
+        }
+        
+        if (key === "ArrowRight") {
+            let nextTab = activeTab.nextElementSibling;
+    
+            if (!nextTab){
+                nextTab = activeTab.parentElement.firstElementChild
+            }
+
+            activeTab.tabIndex = -1;
+            nextTab.tabIndex = 0;
+            nextTab.focus();
+        }
+
+        if (key === "Home") {
+            let previousTab = activeTab.parentElement.firstElementChild;
+
+            activeTab.tabIndex = -1;
+            previousTab.tabIndex = 0;
+            previousTab.focus();
+
+            console.log(key)
+        }
+
+        if (key === "End") {
+            let nextTab = activeTab.parentElement.lastElementChild;
+
+            activeTab.tabIndex = -1;
+            nextTab.tabIndex = 0;
+            nextTab.focus();
+        }
+
+
+        if (key === "Enter" || key === " ") {
+            tab.click();
+        }
+
+
+
+        console.log(event)
+    })
 });
